0b9abd3c95222d02b812efff3ad6f84cc6d09e8e
gralweb <gralweb21@gmail.com>
2021-07-22 12:48:21 -0430

    optimizando codigo para no crear mas funciones identicas para tener multiples barras animadas

--- js/index.js
+++ js/index.js
@@ -1,85 +1,51 @@
-const barr_width = document.querySelector('#barr-width')
-const barr_counter_text = document.querySelector('.barr-counter-text')
-
-const barr_width_dos = document.querySelector('#barr-width-dos')
-const barr_counter_text_dos = document.querySelector('.barr-counter-text-dos')
-
 // Function for change value of the bars
-const barr_num_add = (val, static) => {
-	const barr_form = document.querySelector('#barr-form')
+const barr_num_add = (val, { extend, static }) => {
+	const barr_form = document.querySelector(`#barr-form-${extend}`)
+	const barr_width = document.querySelector(`#barr-width-${extend}`)
+	const barr_counter_text = document.querySelector(`.barr-counter-text-${extend}`)
 
-	barr_width.classList = []
-	barr_width.style.width = `${val}%`
-	barr_counter_text.textContent = val
+	if ( barr_width && barr_counter_text ) {
+		barr_width.classList = []
+		barr_width.style.width = `${val}%`
+		barr_counter_text.textContent = val
+	}
 
-	if ( static ) {
+	if ( static && barr_form ) {
 		barr_form[0].value = val
 	}
 }
 
 // Function for recibe new value for bars and calback for function of the change value
-const barr_changes = () => {
-	const barr_form = document.querySelector('#barr-form')
+const barr_changes = extend => {
+	const barr_form = document.querySelector(`#barr-form-${extend}`)
 
 	barr_form.addEventListener("submit", e => {
 		e.preventDefault()
 
 		const barr_new_value = e.target[0].value
 		
-		barr_num_add(barr_new_value)
+		barr_num_add(barr_new_value, { extend })
 	})
 
 	barr_form[0].onchange = e => {
 		const barr_new_value = e.target.value
 
-		barr_num_add(barr_new_value)
-	}
-
-}
-
-// Clone function 1
-const barr_num_add_dos = (val, static) => {
-	const barr_form_dos = document.querySelector('#barr-form-dos')
-
-	barr_width_dos.classList = []
-	barr_width_dos.classList.add(`barr-width-${val}`)
-	barr_counter_text_dos.textContent = val
-
-	if ( static ) {
-		barr_form_dos[0].value = val
-	}
-}
-
-// Clone function 2
-const barr_changes_dos = () => {
-	const barr_form_dos = document.querySelector('#barr-form-dos')
-
-	barr_form_dos.addEventListener("submit", e => {
-		e.preventDefault()
-
-		const barr_new_value = e.target[0].value
-		
-		barr_num_add_dos(barr_new_value)
-	})
-
-	barr_form_dos[0].onchange = e => {
-		const barr_new_value = e.target.value
-
-		barr_num_add_dos(barr_new_value)
+		barr_num_add(barr_new_value, { extend })
 	}
 
 }
 
 // Function for content the initialice global functions 
-const initialice_funct = () => {
-	barr_changes()
-	barr_changes_dos()
+const initialize_funct = () => {
+	// initialize function for add listener to form for change bars
+	barr_changes('uno')
+	barr_changes('dos')
 
 	// test for auto change number on load windows
-	barr_num_add(50, true)
-	barr_num_add_dos(80, true)
+	barr_num_add(50, { extend: 'uno', static: true })
+	barr_num_add(80, { extend: 'dos', static: true })
 }
 
 window.onload = () => {
-	initialice_funct()
+	initialize_funct()
 }
